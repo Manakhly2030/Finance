2a41b6ff3f17d0b5549715ecb0b2a3e5c23a4cee
Ubuntu <ubuntu@ip-172-31-18-97.ap-south-1.compute.internal>
2018-02-20 05:04:58 +0000

    finance/api.py

--- finance/api.py
+++ finance/api.py
@@ -3,7 +3,8 @@ from __future__ import unicode_literals
 import frappe
 from frappe import _
 from frappe.contacts.doctype.contact.contact import get_contact_details, get_default_contact
-
+from frappe.model.mapper import get_mapped_doc
+from frappe.utils import flt
 
 @frappe.whitelist()
 def install_on_submit(self, method):
@@ -66,3 +67,76 @@ def set_contact_details(out, party, party_type):
 		})
 	else:
 		out.update(get_contact_details(out.contact_person))
+		
+@frappe.whitelist()
+def make_purchase_order_for_drop_shipment(source_name, for_supplier, target_doc=None):
+	def set_missing_values(source, target):
+		target.supplier = for_supplier
+		target.apply_discount_on = ""
+		target.additional_discount_percentage = 0.0
+		target.discount_amount = 0.0
+
+		default_price_list = frappe.get_value("Supplier", for_supplier, "default_price_list")
+		if default_price_list:
+			target.buying_price_list = default_price_list
+
+		if any( item.delivered_by_supplier==1 for item in source.items):
+			if source.shipping_address_name:
+				target.shipping_address = source.shipping_address_name
+				target.shipping_address_display = source.shipping_address
+			else:
+				target.shipping_address = source.customer_address
+				target.shipping_address_display = source.address_display
+
+			target.customer_contact_person = source.contact_person
+			target.customer_contact_display = source.contact_display
+			target.customer_contact_mobile = source.contact_mobile
+			target.customer_contact_email = source.contact_email
+
+		else:
+			target.customer = ""
+			target.customer_name = ""
+
+		target.run_method("set_missing_values")
+		target.run_method("calculate_taxes_and_totals")
+
+	def update_item(source, target, source_parent):
+		target.schedule_date = source.delivery_date
+		target.qty = flt(source.qty)
+		target.stock_qty = (flt(source.qty) - flt(source.ordered_qty)) * flt(source.conversion_factor)
+
+	doclist = get_mapped_doc("Sales Order", source_name, {
+		"Sales Order": {
+			"doctype": "Purchase Order",
+			"field_no_map": [
+				"address_display",
+				"contact_display",
+				"contact_mobile",
+				"contact_email",
+				"contact_person",
+				"taxes_and_charges"
+			],
+			"validation": {
+				"docstatus": ["=", 1]
+			}
+		},
+		"Sales Order Item": {
+			"doctype": "Purchase Order Item",
+			"field_map":  [
+				["name", "sales_order_item"],
+				["parent", "sales_order"],
+				["stock_uom", "stock_uom"],
+				["uom", "uom"],
+				["conversion_factor", "conversion_factor"],
+				["delivery_date", "schedule_date"]
+			],
+			"field_no_map": [
+				"rate",
+				"price_list_rate"
+			],
+			"postprocess": update_item,
+		}
+	}, target_doc, set_missing_values)
+
+	return doclist
+
